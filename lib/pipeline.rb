2602c96087c2c52d2e1ee8afe421eb6309bb9e13
ParadoxV5 <paradox.ver5@gmail.com>
2023-01-25 23:39:36 -0700

    Implement `#then_call` and `#!`

--- lib/pipeline.rb
+++ lib/pipeline.rb
@@ -1,1 +1,12 @@
 require_relative 'pipeline/version'
+
+module Pipeline; refine Object do
+  def then_call(prc = nil) = self.then(&prc)
+  def !(*prcs)
+    if prcs.empty?
+      super
+    else
+      prcs.reduce(self) { _1.then_call(_2) }
+    end
+  end
+end end
