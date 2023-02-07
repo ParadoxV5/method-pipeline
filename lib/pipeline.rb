5c8fe2a8b5476e8f4688d2fe38e579b2100177ab
ParadoxV5 <paradox.ver5@gmail.com>
2023-02-06 22:41:54 -0700

    `prcs` ➡ `procs`

--- lib/pipeline.rb
+++ lib/pipeline.rb
@@ -1,7 +1,7 @@
 require_relative 'pipeline/version'
 
 module Pipeline; refine Object do
-  def then_pipe(*prcs) = prcs.reduce(self) { _1.then(&_2) }
+  def then_pipe(*procs) = procs.reduce(self) { _1.then(&_2) }
   
   alias sys `
   alias ` public_method
